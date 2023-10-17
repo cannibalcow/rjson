a91abb1f05f177cf47e360f7eb5a4a770313a01d
heldt <danielheldtsmail@gmail.com>
2023-10-18 00:23:51 +0200

    soova

---
+++ src/lib.rs
@@ -0,0 +1,233 @@
+#[derive(Debug, PartialEq)]
+enum JsonValue {
+    Bool(bool),
+    Number(f64),
+    Null,
+    Array(Vec<JsonValue>),
+    //   String(String),
+    // Object(HashMap<String, JsonValue>),
+}
+
+#[derive(Debug)]
+enum JsonError {
+    InvalidFormat(String),
+    EndOfStr,
+}
+
+struct Json {
+    pos: usize,
+    json: String,
+}
+
+trait JsonParser {
+    fn parse(&mut self) -> Result<JsonValue, JsonError>;
+    fn parse_bool(&mut self) -> Result<JsonValue, JsonError>;
+    fn parse_number(&mut self) -> Result<JsonValue, JsonError>;
+    fn parse_null(&mut self) -> Result<JsonValue, JsonError>;
+    fn parse_array(&mut self) -> Result<JsonValue, JsonError>;
+}
+
+impl Json {
+    fn new(json: String) -> Self {
+        Self { pos: 0, json }
+    }
+
+    fn consume_char(&mut self) {
+        self.pos += 1;
+    }
+
+    fn consume_chars(&mut self, n: usize) {
+        self.pos += n
+    }
+
+    fn current_char(&self) -> Option<char> {
+        self.json.chars().nth(self.pos)
+    }
+
+    fn next_char(&self) -> Option<char> {
+        self.json.chars().nth(self.pos + 1)
+    }
+
+    fn look_ahead(&self, length: usize) -> String {
+        self.json
+            .chars()
+            .into_iter()
+            .skip(self.pos + 1)
+            .into_iter()
+            .take(length)
+            .collect()
+    }
+
+    fn invalid_format(&self, invalid: char) -> JsonError {
+        JsonError::InvalidFormat(format!(
+            "Invalid format at pos {} found: '{}'",
+            self.pos, invalid
+        ))
+    }
+}
+
+impl JsonParser for Json {
+    fn parse(&mut self) -> Result<JsonValue, JsonError> {
+        match self.current_char() {
+            // Boolean
+            Some('T') | Some('F') | Some('t') | Some('f') => self.parse_bool(),
+            // Number
+            Some('-') | Some('0') | Some('1') | Some('2') | Some('3') | Some('4') | Some('5')
+            | Some('6') | Some('7') | Some('8') | Some('9') => self.parse_number(),
+            // Object
+            Some('N') | Some('n') => self.parse_null(),
+            Some(c) => Err(self.invalid_format(c)),
+            None => Err(JsonError::EndOfStr),
+        }
+    }
+
+    fn parse_bool(&mut self) -> Result<JsonValue, JsonError> {
+        match self.current_char() {
+            Some('t') | Some('T') => match self.look_ahead(3).to_lowercase().as_str() {
+                "rue" => {
+                    self.consume_chars(4);
+                    Ok(JsonValue::Bool(true))
+                }
+                c => Err(JsonError::InvalidFormat(format!(
+                    "Invalid boolean at {} found char: '{}'",
+                    self.pos, c
+                ))),
+            },
+            Some('f') | Some('F') => match self.look_ahead(4).to_lowercase().as_str() {
+                "alse" => {
+                    self.consume_chars(5);
+                    Ok(JsonValue::Bool(false))
+                }
+                c => Err(JsonError::InvalidFormat(format!(
+                    "Invalid boolean at {} found char: '{}'",
+                    self.pos, c
+                ))),
+            },
+            None => Err(JsonError::EndOfStr),
+            c => Err(JsonError::InvalidFormat(format!(
+                "Invalid boolean at {} found char: '{:?}'",
+                self.pos, c
+            ))),
+        }
+    }
+
+    fn parse_number(&mut self) -> Result<JsonValue, JsonError> {
+        let num: String = self
+            .json
+            .chars()
+            .into_iter()
+            .skip(self.pos)
+            .into_iter()
+            .take_while(|c| c.is_digit(10) || *c == '.' || *c == '-')
+            .collect::<String>();
+
+        match num.parse::<f64>() {
+            Ok(v) => {
+                self.consume_chars(num.len());
+                Ok(JsonValue::Number(v))
+            }
+            Err(e) => Err(JsonError::InvalidFormat(format!(
+                "Invalid number format at {} '{}'",
+                self.pos, e
+            ))),
+        }
+    }
+
+    fn parse_null(&mut self) -> Result<JsonValue, JsonError> {
+        match self.look_ahead(3).as_str() {
+            "ull" => {
+                self.consume_chars(4);
+                Ok(JsonValue::Null)
+            }
+            c => Err(JsonError::InvalidFormat(format!(
+                "Invalid null value at {} '{}'",
+                self.pos, c
+            ))),
+        }
+    }
+
+    fn parse_array(&mut self) -> Result<JsonValue, JsonError> {
+        Ok(JsonValue::Array(vec![]))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::collections::HashMap;
+
+    use crate::{Json, JsonParser, JsonValue};
+
+    #[test]
+    fn parse_null() {
+        let data = "null".to_string();
+        let mut json = Json::new(data);
+        assert_eq!(json.parse_null().unwrap(), JsonValue::Null);
+    }
+
+    #[test]
+    #[should_panic]
+    fn parse_knull() {
+        let datar2 = "nkll".to_string();
+        let mut json2 = Json::new(datar2);
+        json2.parse_null().unwrap();
+    }
+
+    #[test]
+    fn parse_num() {
+        let data = "1234".to_string();
+        let mut json = Json::new(data);
+        assert_eq!(json.parse_number().unwrap(), JsonValue::Number(1234.0));
+    }
+
+    #[test]
+    fn parse_num_single() {
+        let data = "2".to_string();
+        let mut json = Json::new(data);
+        assert_eq!(json.parse_number().unwrap(), JsonValue::Number(2.0));
+    }
+
+    #[test]
+    fn parse_num_negative() {
+        let data = "-122".to_string();
+        let mut json = Json::new(data);
+        assert_eq!(json.parse_number().unwrap(), JsonValue::Number(-122.0));
+    }
+
+    #[test]
+    fn parse_num_fraction() {
+        let data = "12.34".to_string();
+        let mut json = Json::new(data);
+        assert_eq!(json.parse_number().unwrap(), JsonValue::Number(12.34));
+    }
+
+    #[test]
+    fn parse_num_leading_zero() {
+        let data = "02".to_string();
+        let mut json = Json::new(data);
+        assert_eq!(json.parse_number().unwrap(), JsonValue::Number(2.0));
+    }
+
+    #[test]
+    fn parse_bool() {
+        let mut cases = HashMap::new();
+        cases.insert("true", JsonValue::Bool(true));
+        cases.insert("false", JsonValue::Bool(false));
+        cases.insert("FALSE", JsonValue::Bool(false));
+        cases.insert("TrUe", JsonValue::Bool(true));
+
+        for key in cases.keys() {
+            cases.get(*key);
+            let mut json = Json::new(key.to_string());
+            assert_eq!(cases.get(*key).unwrap(), &json.parse().unwrap());
+        }
+    }
+
+    #[test]
+    fn look_ahed() {
+        let testdata = "12345";
+        let json = Json::new(testdata.to_string());
+
+        assert_eq!(json.look_ahead(3), String::from("234"));
+        assert_eq!(json.look_ahead(10), String::from("2345"));
+    }
+}
